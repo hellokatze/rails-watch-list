19663d97e800d7e9fe9a916bcc8a7de8682f810f
Katharine Lin <katharine.exe@gmail.com>
2023-05-25 17:23:30 +0200

    backend more or less done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,11 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show edit update]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
   end
 
   def new
@@ -17,9 +18,21 @@ class ListsController < ApplicationController
     redirect_to list_path(@list)
   end
 
+  def edit
+  end
+
+  def update
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: %i[index show new create] do
-    resources :bookmarks, only: %i[new create delete]
+  resources :lists, only: %i[index show new create edit update] do
+    resources :bookmarks, only: %i[new create edit update]
   end
+
+  resources :bookmarks, only: [:destroy]
 end
